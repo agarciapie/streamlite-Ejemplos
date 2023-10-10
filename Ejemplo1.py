6b52907af9cefc278a2941dc5893b57ebe5762e9
Antoni Garcia Pie <63788393+agarciapie@users.noreply.github.com>
2023-10-10 22:08:03 +0200

    Add files via upload

---
+++ Ejemplo1.py
@@ -0,0 +1,16 @@
+
+import streamlit as st
+from PIL import Image
+
+
+
+#config
+st.set_page_config(page_title="Ejemplo1", page_icon="🤖", layout="wide")
+
+with st.container():
+    st.subheader("Hola, somos Valerapp :wave:")
+    st.title("Creamos soluciones para acelerar tu negocio")
+    st.write(
+        "Somos unos apasionados de la tecnología y la innovación, especializados en el sector de la digitalización y automatización de negocios. Nos gusta crear soluciones para resolver problemas y mejorar procesos."
+    )
+    st.write("[Saber más >](https://valerapp.com/)")
