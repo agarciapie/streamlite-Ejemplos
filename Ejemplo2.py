4d33bde8a555e54ca1f85941a9201f1de8f0d1cf
Antoni Garcia Pie <63788393+agarciapie@users.noreply.github.com>
2023-10-11 12:42:36 +0200

    Add files via upload

---
+++ Ejemplo2.py
@@ -0,0 +1,15 @@
+import streamlit as st
+from PIL import Image
+
+
+#config
+st.set_page_config(page_title="Ejemplo2", page_icon="🤖", layout="wide")
+
+
+with st.container():
+    st.subheader("Equipo Pitch&Putt Seniors Canal Olimpic :wave:")
+    st.title("Jugamos la liga SEMAR en 3ª división")
+    st.write(
+        "Somos unos apasionados del deporte del Pitch & Putt."
+    )
+    st.write("[Saber más >](http://www.pipcat.com/seniorsponent/index.php?mod=pip&op=LlistarPag&fid=1")
