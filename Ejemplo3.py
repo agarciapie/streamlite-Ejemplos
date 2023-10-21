2a09a2b94d0c95df8746ef278b7e3c2781386045
Antoni Garcia Pie <63788393+agarciapie@users.noreply.github.com>
2023-10-21 20:54:54 +0200

    Nada

--- Ejemplo3.py
+++ Ejemplo3.py
@@ -10,7 +10,7 @@ st.set_page_config(
     page_icon="⛳", 
     layout="centered"
     menu_items= {
-         "About": "Es una pagimna de prueba"
+         'About': "Es una pagimna de prueba"
     }
        
     )
