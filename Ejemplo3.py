4cfa885a5b02f452397687911d1b236968d19ffe
Antoni Garcia Pie <63788393+agarciapie@users.noreply.github.com>
2023-10-22 18:09:21 +0200

    res

--- Ejemplo3.py
+++ Ejemplo3.py
@@ -32,6 +32,7 @@ st.button('Click me', on_click=click_button)
 if st.session_state.button:
     # The message and nested widget will remain on the page
     st.write('Button is on!')
+    st.write('otro texto')
     #st.slider('Select a value')
 else:
     st.write('Button is off!')
