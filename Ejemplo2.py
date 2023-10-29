c83a8df07b54c8533e476fba0f1b73b8652bf3b2
Antoni Garcia Pie <63788393+agarciapie@users.noreply.github.com>
2023-10-29 21:51:52 +0100

    Añadido emoji golf

--- Ejemplo2.py
+++ Ejemplo2.py
@@ -5,7 +5,7 @@ foto_equipo = Image.open("Imagenes/Equip_P&P_2023-24.jpg")
 logo_co = Image.open("Imagenes/logo_equip.JPG")
 
 #config
-st.set_page_config(page_title="Ejemplo2", page_icon="🤖", layout="wide")
+st.set_page_config(page_title="Ejemplo2", page_icon="⛳", layout="wide")
 
 
 with st.container():
