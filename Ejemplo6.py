dd6ae8c95187ca2b5656521e9ff0895c833868ba
Antoni Garcia Pie <63788393+agarciapie@users.noreply.github.com>
2023-11-05 23:54:23 +0100

    Side bar menu

---
+++ Ejemplo6.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+# Ocultar leyenda Streamlit
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
+# 1. as sidebar menu
+with st.sidebar:
+    selected = option_menu("Main Menu", ["Home", 'Settings'], 
+        icons=['house', 'gear'], menu_icon="cast", default_index=1)
+    selected
