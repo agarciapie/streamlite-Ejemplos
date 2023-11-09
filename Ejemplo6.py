446f91edbcaa41a9c8e311292bbc5c5299955704
Antoni Garcia Pie <63788393+agarciapie@users.noreply.github.com>
2023-11-09 17:37:15 +0100

    res

--- Ejemplo6.py
+++ Ejemplo6.py
@@ -23,6 +23,7 @@ def menu_change(key):
         
     if selection == "Resultados":
         #st.write(f"El menu ha cambiado a {selection}")
+        st.header("Resultats")
         st.write("[Veure resultats SP >](http://pitch.cat/noticies/noticia.php?id=46944)")
         st.write("[Veure resultats MP >](http://www.pitchputt.cat/noticies/noticia.php?id=46838)")
 
