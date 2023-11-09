d301dc494924e22c9a896e916902ed347b428e14
Antoni Garcia Pie <63788393+agarciapie@users.noreply.github.com>
2023-11-09 17:41:11 +0100

    afeixejo texte i fotos

--- Ejemplo6.py
+++ Ejemplo6.py
@@ -1,5 +1,12 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
+from PIL import Image
+
+foto_equipo = Image.open("Imagenes/Equip_P&P_2023-24_1.jpg")
+logo_co = Image.open("Imagenes/logo_equip.JPG")
+
+#config
+st.set_page_config(page_title="Senior Olimpics P&P", page_icon="⛳", layout="wide")
 
 # Ocultar leyenda Streamlit
 hide_st_style = """
@@ -10,6 +17,15 @@ hide_st_style = """
             </style>
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
+
+st.image(foto_equipo)
+
+with st.container():
+    left_column, right_column = st.columns([0.7, 0.3])
+    with left_column:
+        st.title("Equip de Pitch&Putt Senior Olimpics")
+    with right_column:
+        st.image(logo_co)
 
 def menu_change(key):
     selection = st.session_state[key]
